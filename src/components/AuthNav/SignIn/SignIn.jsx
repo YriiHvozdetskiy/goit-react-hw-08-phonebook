c7ad0183f3eecdeb015ee48564e20509205cfa82
YriiHvozdetskiy <hvozdetskiyyrii@gmail.com>
2021-09-03 15:16:42 +0300

    add  Sign in

--- src/components/AuthNav/SignIn/SignIn.jsx
+++ src/components/AuthNav/SignIn/SignIn.jsx
@@ -1,8 +1,56 @@
-export const  SignIn = () => {
-    
-    return (
-    <>
-       Sing in (Увійти)
-    </>)
+import {useDispatch} from "react-redux";
+import {useState} from "react";
+import {authOperations} from "../../../redux/auth";
+import s from './SignIn.module.scss'
+
+export const SignIn = () => {
+  // const dispatch = useDispatch();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleChange = ({target: {name, value}}) => {
+	switch (name) {
+	  case 'email':
+		return setEmail(value);
+	  case  'password':
+		return setPassword(value);
+	  default:
+		return;
+	}
+  }
+
+  const handleSubmit = e => {
+	e.preventDefault();
+	// dispatch(authOperations.logIn({email, password}))
+	setEmail('')
+	setPassword('')
+  }
+
+  return (
+	<div>
+	  <h1> Sing in (Увійти)</h1>
+	  <form className={s.form} onSubmit={handleSubmit}>
+		<label className={s.label}>
+		  email
+		  <input type='email'
+				 name='email'
+				 value={email}
+				 onChange={handleChange}
+		  />
+		</label>
+		<label className={s.label}>
+		  password
+		  <input type='password'
+				 name='password'
+				 value={password}
+				 onChange={handleChange}
+		  />
+		</label>
+		<button type='submit'>
+		  Sign in
+		</button>
+	  </form>
+	</div>
+  )
 }
 
