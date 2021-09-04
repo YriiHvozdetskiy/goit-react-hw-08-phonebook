d49229cbcac5625d079b3e321c588a3fe020284b
YriiHvozdetskiy <hvozdetskiyyrii@gmail.com>
2021-09-04 10:28:43 +0300

    impov

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -5,19 +5,18 @@ import s from './AuthNav.module.scss'
 
 export const AuthNav = () => {
   return (
-	<>
-	  <ul className={s.list}>
-		<li className={s.item}>
-		  <NavLink exact to='/register'>
-			Sign Up
-		  </NavLink>
-		</li>
-		<li className={s.item}>
-		  <NavLink to='/login'>
-			Sign In
-		  </NavLink>
-		</li>
-	  </ul>
-	</>)
+	<ul className={s.list}>
+	  <li className={s.item}>
+		<NavLink exact to='/register'>
+		  Sign Up
+		</NavLink>
+	  </li>
+	  <li className={s.item}>
+		<NavLink to='/login'>
+		  Sign In
+		</NavLink>
+	  </li>
+	</ul>
+  )
 }
 
