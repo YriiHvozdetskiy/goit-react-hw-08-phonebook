512bbce05ed4c78676420aa5fe5326e24a31af2b
YriiHvozdetskiy <hvozdetskiyyrii@gmail.com>
2021-09-04 14:30:28 +0300

    add  redux-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,23 @@
+import {persistStore, persistReducer,} from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 import {configureStore} from "@reduxjs/toolkit";
 import {authReducer} from './auth';
 import {contactsReducer} from "./contacts";
 
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
+
+
 export const store = configureStore({
   reducer: {
-	auth: authReducer,
+	// auth: authReducer,
+	auth: persistReducer(authPersistConfig, authReducer),
 	contacts: contactsReducer,
   },
   devTools: process.env.NODE_ENV === 'development',
 })
+
+export const persistor = persistStore(store);
