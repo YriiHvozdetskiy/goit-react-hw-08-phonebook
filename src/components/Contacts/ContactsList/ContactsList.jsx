c3f8055fa9659c1526275bd6600078da321fb13e
YriiHvozdetskiy <hvozdetskiyyrii@gmail.com>
2021-09-04 13:15:44 +0300

    add ContactsList

--- src/components/Contacts/ContactsList/ContactsList.jsx
+++ src/components/Contacts/ContactsList/ContactsList.jsx
@@ -1,39 +1,38 @@
-import { useDispatch, useSelector } from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
+import {useEffect} from 'react';
 import s from './ContactsList.module.scss';
-// import { deleteContact, fetchContactsList } from '../../redux/actions';
-import { useEffect } from 'react';
-// import { getFilteredContacts } from '../../redux/selectors';
+import {contactsOperations, contactsSelectors} from "../../../redux/contacts";
 
-export const ContactsList =() =>{
-  // const dispatch = useDispatch();
-  // const filteredContacts = useSelector(getFilteredContacts);
+export const ContactsList = () => {
+  const dispatch = useDispatch();
+  const filteredContacts = useSelector(contactsSelectors.getFilteredContacts);
 
-  // useEffect(() => {
-	// dispatch(fetchContactsList());
-  // }, [dispatch]);
+  useEffect(() => {
+	dispatch(contactsOperations.fetchContactsList());
+  }, [dispatch]);
 
-  // const removeContact = id => {
-	// dispatch(deleteContact(id));
-  // };
+  const removeContact = id => {
+	dispatch(contactsOperations.deleteContact(id));
+  };
 
   return (
 	// рендерем контакти з масиву filteredContacts || рендерем результати пошуку
 	<ul className={s.list}>
-	  {/*{filteredContacts &&*/}
-	  {/*filteredContacts.map(({ id, name, number }) => {*/}
-		{/*return (*/}
-		{/*  <li className={s.item} key={id}>*/}
-		{/*	<span>{name}</span>: <span>{number}</span>*/}
-		{/*	<button*/}
-		{/*	  className={s.button}*/}
-		{/*	  type="button"*/}
-		{/*	  // onClick={() => removeContact(id)}*/}
-		{/*	>*/}
-		{/*	  Delete*/}
-		{/*	</button>*/}
-		{/*  </li>*/}
-		{/*);*/}
-	  {/*})}*/}
+	  {filteredContacts &&
+	  filteredContacts.map(({id, name, number}) => {
+		return (
+		  <li className={s.item} key={id}>
+			<span>{name}</span>: <span>{number}</span>
+			<button
+			  className={s.button}
+			  type="button"
+			  onClick={() => removeContact(id)}
+			>
+			  Delete
+			</button>
+		  </li>
+		);
+	  })}
 	</ul>
   );
 }
