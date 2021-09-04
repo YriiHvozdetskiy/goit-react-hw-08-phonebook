b8002d78fd5919f828840213646f5ea4ad632e7d
YriiHvozdetskiy <hvozdetskiyyrii@gmail.com>
2021-09-04 10:41:24 +0300

    add userMenu

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,8 +1,19 @@
-export const  UserMenu = () => {
-    
-    return (
-    <>
-        User Menu
-    </>)
+import {useDispatch, useSelector} from "react-redux";
+import {authOperations, authSelectors} from "../../redux/auth";
+import s from './UserMenu.module.scss';
+import defaultAvatar from './avatar.jpg';
+
+export const UserMenu = () => {
+  const name = useSelector(authSelectors.getUserName);
+  const dispatch = useDispatch();
+  const avatar = defaultAvatar;
+
+  return (
+	<div className={s.container}>
+	  <img className={s.avatar} src={avatar} alt="avatar"/>
+	  <span className={s.name}>Welcome {name}</span>
+	  <button type='button' onClick={() => dispatch(authOperations.logOut())}>Log out</button>
+	</div>
+  )
 }
 
