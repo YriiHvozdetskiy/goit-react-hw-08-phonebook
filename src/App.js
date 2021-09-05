ecc5fbc03e3e7905dc1e1b48356f91e6401f881d
YriiHvozdetskiy <hvozdetskiyyrii@gmail.com>
2021-09-05 11:46:08 +0300

    зробили щоб не мигав інтерфейс

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import {Container} from "./components/Container/Container";
 import {Header} from "./components/Header/Header";
 import {Main} from "./components/Main/Main";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {useEffect} from "react";
-import {authOperations} from "./redux/auth";
+import {authOperations, authSelectors} from "./redux/auth";
 
 export default function App() {
   const dispatch = useDispatch();
+  const isFetchingCurrent = useSelector(authSelectors.getIsFetchingCurrent);
 
   useEffect(() => {
 	dispatch(authOperations.fetchCurrentUser());
   }, [dispatch]);
-
+//TODO додати loader чи скілетон
   return (
-	<>
-	  <Container>
-		<Header/>
-		<Main/>
-	  </Container>
-	</>
+	!isFetchingCurrent &&
+	(<Container>
+	  <Header/>
+	  <Main/>
+	</Container>)
   );
 }
 
